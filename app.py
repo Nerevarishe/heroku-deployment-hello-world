92d4f9240253d40b59c9a5a7b8fccbdf719cd04f
Link <Nerevarishe@users.noreply.github.com>
2020-07-26 13:34:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask
 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key'
+
+
+@app.route('/', methods=['GET'])
+def default_route():
+    return '<h1>Hello world from Heroku</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
