8dfa01dd7d96bd625f81a56ba4691351a58dbad7
Link <Nerevarishe@users.noreply.github.com>
2020-07-26 14:44:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,3 @@ CORS(app)
 @app.route('/', methods=['GET'])
 def default_route():
     return '<h1>Hello world from Heroku</h1>'
-
-#app.run()
-#app.run(host='0.0.0.0')
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0')
